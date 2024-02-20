17d582e8d7e67dad5b022e13cb330cb3894b6af1
egamovj <egamovj90@gmail.com>
2024-02-20 12:28:22 +0500

    added visitors

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,8 +1,41 @@
+import AnalyticsDashboard from "@/components/AnalyticsDashboard";
+import { getDate } from "@/utils";
 import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
-  const pageview = await analytics.retrieve("pageview", "20/02/2024");
+  const TRACKING_DAYS = 7;
 
-  return <pre>{JSON.stringify(pageview)}</pre>;
+  const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS);
+
+  const totalPageViews = pageviews.reduce((acc, curr) => {
+    return (
+      acc +
+      curr.events.reduce((acc, curr) => {
+        return acc + Object.values(curr)[0]!;
+      }, 0)
+    );
+  }, 0);
+
+  const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1);
+
+  const amtVisitorsToday = pageviews
+    .filter((ev) => ev.date === getDate())
+    .reduce((acc, curr) => {
+      return (
+        acc +
+        curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0)
+      );
+    }, 0);
+
+  return (
+    <div className="min-h-screen w-full py-12 flex justify-center items-center">
+      <div className="relative w-full max-w-6xl mx-auto text-white">
+        <AnalyticsDashboard
+          avgVisitorsPerDay={avgVisitorsPerDay}
+          amtVisitorsToday={amtVisitorsToday}
+        />
+      </div>
+    </div>
+  );
 };
 export default Page;
---
+++ src/components/AnalyticsDashboard.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Card } from "@tremor/react";
+
+interface AnalyticsDashboardProps {
+  avgVisitorsPerDay: string;
+  amtVisitorsToday: number;
+}
+
+const AnalyticsDashboard = ({
+  avgVisitorsPerDay,
+  amtVisitorsToday,
+}: AnalyticsDashboardProps) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            Avg. visitors/day
+          </p>
+          <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+            {avgVisitorsPerDay}
+          </p>
+        </Card>
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            Visitors today
+          </p>
+          <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+            {amtVisitorsToday}
+          </p>
+        </Card>
+      </div>
+    </div>
+  );
+};
+export default AnalyticsDashboard;
