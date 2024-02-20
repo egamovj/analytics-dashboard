907474dc53c8f3d0149c7d8f93373b92f71af85e
egamovj <egamovj90@gmail.com>
2024-02-20 21:22:30 +0500

    visitor statistics were released in percentage

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -2,6 +2,7 @@
 
 import { analytics } from "@/utils/analytics";
 import { BarChart, Card } from "@tremor/react";
+import { ArrowDownRight, ArrowRight, ArrowUpRight } from "lucide-react";
 import ReactCountryFlag from "react-country-flag";
 
 interface AnalyticsDashboardProps {
@@ -10,6 +11,35 @@ interface AnalyticsDashboardProps {
   timeseriesPageviews: Awaited<ReturnType<typeof analytics.retrieveDays>>;
   topCountries: [string, number][];
 }
+
+const Badge = ({ percentage }: { percentage: number }) => {
+  const isPositive = percentage > 0;
+  const isNeutral = percentage === 0;
+  const isNegative = percentage < 0;
+
+  if (isNaN(percentage)) return null;
+
+  const positiveClassname = "bg-green-900/25 text-green-400 ring-green-400/25";
+  const neutralClassname = "bg-zinc-900/25 text-zinc-400 ring-zinc-400/25";
+  const negativeClassname = "bg-red-900/25 text-red-400 ring-red-400/25";
+
+  return (
+    <span
+      className={`inline-flex gap-1 items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ${
+        isPositive
+          ? positiveClassname
+          : isNeutral
+          ? neutralClassname
+          : negativeClassname
+      }`}
+    >
+      {isPositive ? <ArrowUpRight className="h-3 w-3" /> : null}
+      {isNeutral ? <ArrowRight className="h-3 w-3" /> : null}
+      {isNegative ? <ArrowDownRight className="h-3 w-3" /> : null}
+      {percentage.toFixed(0)}%
+    </span>
+  );
+};
 
 const AnalyticsDashboard = ({
   avgVisitorsPerDay,
@@ -29,8 +59,13 @@ const AnalyticsDashboard = ({
           </p>
         </Card>
         <Card className="w-full mx-auto max-w-xs">
-          <p className="text-tremor-default text-dark-tremor-content">
+          <p className="flex gap-2.5 items-center text-tremor-default text-dark-tremor-content">
             Visitors today
+            <Badge
+              percentage={
+                (amtVisitorsToday / Number(avgVisitorsPerDay) - 1) * 100
+              }
+            />
           </p>
           <p className="text-3xl text-dark-tremor-content-strong font-semibold">
             {amtVisitorsToday}
