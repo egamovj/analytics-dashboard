b226e299fe6f3d55eeb31d47259beba6a4d008e2
egamovj <egamovj90@gmail.com>
2024-02-20 11:24:59 +0500

    added date to upstash base

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,7 +1,12 @@
 import { redis } from "@/lib/redis";
+import { getDate } from "@/utils";
 
 type AnalyticsArgs = {
   retention?: number;
+};
+
+type TrackOptions = {
+  persist?: boolean;
 };
 
 export class Analytics {
@@ -10,9 +15,15 @@ export class Analytics {
   constructor(opts?: AnalyticsArgs) {
     if (opts?.retention) this.retention = opts.retention;
   }
-  async track(namespace: string, event: object = {}) {
-    const key = `analytics::${namespace}`;
+  async track(namespace: string, event: object = {}, opts?: TrackOptions) {
+    let key = `analytics::${namespace}`;
+
+    if (!opts?.persist) {
+      key += `::${getDate()}`;
+    }
+
     await redis.hincrby(key, JSON.stringify(event), 1);
+    if (!opts?.persist) await redis.expire(key, this.retention);
   }
 }
 
